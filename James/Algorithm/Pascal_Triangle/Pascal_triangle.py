0c2f57621f3319127d11c4be52a3f09580149dd4
James Guo <144802763+JaDGu@users.noreply.github.com>
2026-05-23 21:22:57 -0400

    no "*" update; comment update

--- James/Algorithm/Pascal_Triangle/Pascal_triangle.py
+++ James/Algorithm/Pascal_Triangle/Pascal_triangle.py
@@ -1,17 +1,20 @@
 def pascal_triangle(num) :
     triangle = []
+    # 生成杨辉三角数列
     for n in range(num):
-        row = [1] * (n + 1)
+        row = [1 for m in range(n+1)]
         for p in range(1,n):
             row[p] = triangle[n-1][p-1] + triangle[n-1][p]
         triangle.append(row)
-        # print(row)
-    # print(triangle)
+
+    # 生成合适的空格数
     for i in range(num):
+        #数字间的空格
         strnum = ""
         for y in range(i+1):
             strnum += str(triangle[i][y])
             strnum += " "
+        #数字前的空格
         spacect = ""
         for x in range(num - i - 1):
             spacect += " "
