ce0a9f80fdd15425019324d74e6d84caf6df9b1d
ZHENHAO YANG <49468724+Allenyzh@users.noreply.github.com>
2026-08-02 15:33:17 -0400

    Update 07.js

--- Allen/016.sync/07.js
+++ Allen/016.sync/07.js
@@ -5,10 +5,16 @@ function times(a, b, delay) {
   });
 }
 
-function divide(a, b, delay) {
-  const result = a / b;
+function divide(a, b, delay = 1) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(result), delay * 1000);
+    if (b === 0) {
+      reject(new Error("除法分母不能为零"));
+    } else {
+      setTimeout(() => {
+        const result = a / b;
+        resolve(result);
+      }, delay * 1000);
+    }
   });
 }
 
@@ -27,4 +33,28 @@ times(2, 6, 2)
 times(2, 6, 2).then((result) => {
   console.log(`乘法结果是 ${result} [嵌套]`);
   divide(result, 3, 2).then((re) => console.log(`除法结果是 ${re} [嵌套]`));
+});
+
+// nested then catch
+times(2, 6, 2).then((resultTimes) => {
+  console.log(`乘法结果是 ${resultTimes} [嵌套]`);
+  divide(resultTimes, 0, 2)
+    .then((resultDivide) => {
+      console.log(`除法结果是 ${resultDivide} [嵌套]`);
+      return {
+        resultTimes,
+        resultDivide,
+      };
+    })
+    .then((results) => console.log(results))
+    .catch((err) => {
+      console.log(err.message);
+      const fallbackResult = {
+        resultTimes,
+        resultDivide: null,
+      };
+
+      console.log(fallbackResult);
+      return fallbackResult;
+    });
 });
